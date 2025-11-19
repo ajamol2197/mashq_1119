c4fe5cc26fe115dd2e09cebfb152e1678282558c
ajamol2197 <ajamol2197@gmail.com>
2025-11-19 15:37:24 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+# 1-m
+a = "abdefghijk"
+print(a)
+
+t = a[:5] + a[-5:]
+print(t)
+
+# 2-m
+a = "opelalxalgogleal"
+print(a)
+
+soni = a.count("al")
+print(soni)
+res = "al" * soni
+print(res)
+
+# 3-m
+a = "1234567890abc"
+print(a)
+
+t = a[-3:]
+print(t)
+
+# 4-m
+a = "pilandromecheck"
+print(a)
+
+t = a[0:4] + a[::-4]
+print(t)
+
+# 5-m
+a = "supergrok4lshere"
+print(a)
+
+t = a[5:-5]
+print(t)
